18cd9684f70840e4ac9f1cef1c7ae118f64a34e3
Kevin <lopezkevin175@gmail.com>
2018-04-18 13:51:49 -0400

    solved reverseWords problem

---
+++ Problem-1.js
@@ -0,0 +1,14 @@
+/* Write a reverseWords function that accepts a string a parameter,
+   and reverses each word in the string. Any spaces in the string should 
+   be retained. */
+
+const reverseWords = (str) => {
+  let arr = str.split(" "); //splitting str argument to a an array of words
+  let wordsReversed = []; //where our split words will be pushed
+    
+  for(let i = 0; i < arr.length; i++) {
+    wordsReversed.push(arr[i].split("").reverse().join("")); //looping through array of words
+  } //this loop is pushing reversed words into our wordsReversed empty array
+    
+  return wordsReversed.join(" "); // joining our array of split words to a string and voila
+}
