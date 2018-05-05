2ca1c817f7ea98922364fe6c7048c41507bcf02b
Kevin <lopezkevin175@gmail.com>
2018-05-04 21:45:43 -0400

    quick change

--- problem-7.js
+++ problem-7.js
@@ -1,4 +1,5 @@
 const stringTransformer = str => {
   const allUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return str.split('').map(c => allUpperCase.indexOf(c) === -1 ? c.toUpperCase() : c.toLowerCase()).join('').split(' ').reverse().join(' ')
+  return str.split('').map(c => allUpperCase.indexOf(c) === -1 ?
+  c.toUpperCase() : c.toLowerCase()).join('').split(' ').reverse().join(' ')
 }
