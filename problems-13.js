1c49908bcf748f68089fa207ac08f2c574d02f2e
Kevin Lopez <lopezkevin175@gmail.com>
2018-05-09 23:49:22 -0400

    Create problems-13.js

---
+++ problems-13.js
@@ -0,0 +1,7 @@
+//Find the total sum of angles in an n sided shape. N will be greater than 2.
+/*The formula for calculating the sum of the interior angles of a regular polygon is:
+(n - 2) × 180° where n is the number of sides of the polygon.
+This formula comes from dividing the polygon up into triangles using full diagonals.
+We already know that the interior angles of a triangle add up to 180°.*/
+
+const angle = n => {return (n - 2) * 180};
