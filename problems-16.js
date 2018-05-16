3ab8a319674421d2ccb820b048b56244970f06c8
Kevin Lopez <lopezkevin175@gmail.com>
2018-05-16 18:57:34 -0400

    Create problems-16.js

---
+++ problems-16.js
@@ -0,0 +1,15 @@
+/*You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: array may be empty, in this case return 0.*/
+
+const positiveSum = arr => {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0){
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
