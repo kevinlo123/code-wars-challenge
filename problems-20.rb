7da495b805939bc62d9aa8d729ebf3aadc317bf6
Kevin Lopez <lopezkevin175@gmail.com>
2018-05-19 17:31:27 -0400

    Create problems-20.rb

---
+++ problems-20.rb
@@ -0,0 +1,10 @@
+=begin Very simple, given a number, find its opposite.
+Examples:
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals? =end
+
+def opposite(num)
+  return num * -1
+end
