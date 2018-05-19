750aafd61fcf078d4e20a430aec63cb8e4374465
Kevin Lopez <lopezkevin175@gmail.com>
2018-05-19 17:32:24 -0400

    Update problems-20.rb

--- problems-20.rb
+++ problems-20.rb
@@ -1,9 +1,11 @@
-=begin Very simple, given a number, find its opposite.
+=begin
+Very simple, given a number, find its opposite.
 Examples:
 1: -1
 14: -14
 -34: 34
-But can you do it in 1 line of code and without any conditionals? =end
+But can you do it in 1 line of code and without any conditionals?
+=end
 
 def opposite(num)
   return num * -1
