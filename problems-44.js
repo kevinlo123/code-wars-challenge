3244fab7ded73e9bce507d006d40624d2385c3ce
Kevin Lopez <lopezkevin175@gmail.com>
2018-10-02 23:30:50 -0400

    Create problems-44.js

---
+++ problems-44.js
@@ -0,0 +1,12 @@
+/*Write a function called repeatStr which repeats the given string string exactly n times.
+
+repeatStr(6, "I") // "IIIIII"
+repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"*/
+
+const repeatStr = (n, s) => {
+  let res = "";
+  for(let i = 0; i < n; i++) {
+    res += s
+  }
+  return res;
+}
