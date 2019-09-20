956485075833f05b5d6599e2651677b379e091d5
Kevin Lopez <lopezkevin175@gmail.com>
2019-09-20 10:45:48 -0400

    Create problems-51.js

---
+++ problems-51.js
@@ -0,0 +1,28 @@
+/*Given an array, find the int that appears an odd number of times.
+There will always be only one integer that appears an odd number of times.*/
+
+function findOdd(A) {
+  let resArr = [];
+  let answer;
+  
+  const countInArray = (array, value) => {
+    return array.reduce((n, x) => n + (x === value), 0);
+  }
+  
+  A.map(num => {
+    let count = countInArray(A, num);
+    resArr.push({
+      num: num,
+      timesShown: count
+    });
+  });
+  
+  resArr.filter( numObj => {
+	  if (numObj.timesShown % 2) {
+      answer = numObj.num
+      return numObj.num
+    }
+  });
+  
+  return answer;
+ }
